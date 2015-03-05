f9a435fbb0ffc20b49efa1130e770bdb74f0caf3
nakajmg <nakajima.jmg@gmail.com>
2015-03-06 05:15:49 +0900

    add demo filter

---
+++ 9/filter/1/app.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+  el: '#app',
+  data: {
+    people: [
+      {name: 'nakajmg', count: 1},
+      {name: 'hokaccha', count: 2},
+      {name: 'takazudo', count: 3},
+      {name: 'yomotsu', count: 4},
+      {name: 'ykhs', count: 5}
+    ]
+  }
+});
